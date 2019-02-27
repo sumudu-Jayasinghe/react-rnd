3ac5cec433de3db5098bb636c14c2d34b46a3820
Sumudu Jayasinghe <sumjayasinghe@virtusa.com>
2019-02-27 17:51:04 +0530

    ADD BUILD

---
+++ precache-manifest.c4f9b92d6757f0168aa986d237300ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dcef327d06001cc45dbf",
+    "url": "/static/js/main.dcef327d.chunk.js"
+  },
+  {
+    "revision": "4d23f7780bbb7079c577",
+    "url": "/static/js/2.4d23f778.chunk.js"
+  },
+  {
+    "revision": "dcef327d06001cc45dbf",
+    "url": "/static/css/main.830aa025.chunk.css"
+  },
+  {
+    "revision": "92f8dd47ff37438eb4c4458594fdfee6",
+    "url": "/index.html"
+  }
+];
--- src/games/GamesHome.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const GamesHome = ()=>{
-    return (
-        <div>Games home</div>
-    )
-}
