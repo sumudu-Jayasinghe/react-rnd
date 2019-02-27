58aa572fa31cf6dccafe6448d215a65ea0de6fe3
Sumudu Jayasinghe <sumjayasinghe@virtusa.com>
2019-02-27 17:43:52 +0530

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { UserView } from './git-users/UserView';
+import { Topnav } from './layout/TopNav';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Home } from './home/Home';
+import { RndHome } from './rnd/RndHome';
+import { GamesHome } from './games/GamesHome';
+import { UserView } from './rnd/git-users/UserView';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <nav className='navbar fixed-top navbar-dark bg-primary'>
-          <a className='navbar-brand'>React</a>
-        </nav>
-        <div className='content' style={{ marginTop: '60px' }}>
-          <UserView />
+      <Router>
+        <div className='container'>
+          <nav
+            className='navbar fixed-top navbar-dark'
+            style={{
+              backgroundColor: '#eeeeee',
+              borderBottom: 'solid 1px #c3c3c3'
+            }}
+          >
+            <a className='navbar-brand'>React Labs</a>
+            <Topnav />
+          </nav>
+          <div className='content' style={{ marginTop: '60px' }}>
+            <Route path='/' exact component={Home} />
+            <Route path='/rnd' exact component={RndHome} />
+            <Route path='/rnd/git' exact component={UserView} />
+            <Route path='/games' exact component={GamesHome} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/games/GamesHome.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const GamesHome = ()=>{
+    return (
+        <div>Games home</div>
+    )
+}
